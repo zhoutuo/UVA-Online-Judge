a52ea9681dd8024ed9e3d911aa24ebee1967f390
eclipse9614 <yzt25864126@gmail.com>
2012-06-28 11:32:12 +0800

    Still 0.08s after changing the size of buffer

--- 272 - TEX Quotes.cpp
+++ 272 - TEX Quotes.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     char tmp;
-    char buffer[100];
+    char buffer[2000];
     int count = 0;
     bool flag = false; // false means `` true means ''
 
@@ -31,7 +31,7 @@ int main()
             buffer[count++] = tmp;
         }
 
-        if(count > 90)
+        if(count > 1990)
         {
             buffer[count] = '\0';
             printf("%s", buffer);
