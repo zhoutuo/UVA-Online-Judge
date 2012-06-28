eafc56123cab08c3a5c369252c0ff84a1243aef1
eclipse9614 <yzt25864126@gmail.com>
2012-06-28 10:49:41 +0800

    272 - TEX Quotes 3372	10266018	0.008	C++	2012-06-28 02:44:03

---
+++ 272 - TEX Quotes.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+using namespace std;
+
+
+
+int main()
+{
+    char tmp;
+    char buffer[100];
+    int count = 0;
+    bool flag = false; // false means `` true means ''
+
+    while(scanf("%c", &tmp) != EOF)
+    {
+        if(tmp == '"')
+        {
+            if(!flag)
+            {
+                buffer[count++] = '`';
+                buffer[count++] = '`';
+            }
+            else
+            {
+                buffer[count++] = '\'';
+                buffer[count++] = '\'';
+            }
+            flag = !flag;
+        }
+        else
+        {
+            buffer[count++] = tmp;
+        }
+
+        if(count > 90)
+        {
+            buffer[count] = '\0';
+            printf("%s", buffer);
+            count = 0;
+        }
+    }
+
+    buffer[count] = '\0';
+    printf("%s", buffer);
+
+
+}
