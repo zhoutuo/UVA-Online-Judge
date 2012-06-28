79de906ffc2f650147a8e4627fc0e1b879a972e0
eclipse9614 <yzt25864126@gmail.com>
2012-06-28 09:32:01 +0800

    Time reduces to 0.36s

--- 102 - Ecological Bin Packing.cpp
+++ 102 - Ecological Bin Packing.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main()
 {
-    int bin[3][3];// in "BGC" order
+    int bin[3][3];// in "BGC" color
+    int moves[3][3];// in "BGC" color
+
     char finalOrder[4];
     finalOrder[3] = '\0';
     int finalMoves;
-    char defaultColor[3] = {'B', 'G', 'C'};
-    char candidateOrder[6][3] = {{'B', 'C', 'G'}, {'B', 'G', 'C'},
-    {'C', 'B', 'G'}, {'C', 'G', 'B'}, {'G', 'B', 'C'}, {'G', 'C', 'B'}};
+    //char defaultColor[3] = {'B', 'G', 'C'};
     int potentialMoves;
+
     while(scanf("%d", &bin[0][0]) != EOF)
     {
         scanf("%d %d %d %d %d %d %d %d", &bin[0][1], &bin[0][2], &bin[1][0], &bin[1][1],
@@ -18,33 +19,87 @@ int main()
 
         finalMoves = 900000000;
 
-        for(int i = 0; i < 6; ++i)
+        //calculate the matrix of movement
+        //input: 1 2 3 4 5 6 7 8 9
+        //if the first bin is B, the usage is 5, G => 4, C => 3
+        //if the second B => 11 G =>10 C => 9
+        //if third B => 17 G => 16 C => 15
+        //select the minimal legal combination
+
+        //first bin => second => third
+        for(int i = 0; i < 3; ++i)
         {
-            potentialMoves = 0;
-            for(int j = 0; j < 3; ++j)
-            {
-                for(int z = 0; z < 3; ++z)
-                {
-                    if(candidateOrder[i][j] != defaultColor[z])
-                    {
-                        potentialMoves += bin[j][z];
-
-                    }
-                }
-            }
-
-            if(potentialMoves < finalMoves)
-            {
-                finalMoves = potentialMoves;
-                for(int j = 0; j < 3; ++j)
-                {
-                    finalOrder[j] = candidateOrder[i][j];
-                }
-
-
-            }
+            moves[i][0] = bin[i][1] + bin[i][2];
+            moves[i][1] = bin[i][0] + bin[i][2];
+            moves[i][2] = bin[i][0] + bin[i][1];
         }
 
+        //selection, color order is bgc
+        //bcg
+        potentialMoves = (moves[0][0] + moves[1][2] + moves[2][1]);
+        if(potentialMoves < finalMoves)
+        {
+            finalMoves = potentialMoves;
+            finalOrder[0] = 'B';
+            finalOrder[1] = 'C';
+            finalOrder[2] = 'G';
+        }
+
+
+        //bgc
+        potentialMoves = (moves[0][0] + moves[1][1] + moves[2][2]);
+        if(potentialMoves < finalMoves)
+        {
+            finalMoves = potentialMoves;
+            finalOrder[0] = 'B';
+            finalOrder[1] = 'G';
+            finalOrder[2] = 'C';
+
+        }
+
+
+        //CBG
+        potentialMoves = (moves[0][2] + moves[1][0] + moves[2][1]);
+        if(potentialMoves < finalMoves)
+        {
+            finalMoves = potentialMoves;
+            finalOrder[0] = 'C';
+            finalOrder[1] = 'B';
+            finalOrder[2] = 'G';
+
+        }
+
+
+        //CGB
+        potentialMoves = (moves[0][2] + moves[1][1] + moves[2][0]);
+        if(potentialMoves < finalMoves)
+        {
+            finalMoves = potentialMoves;
+            finalOrder[0] = 'C';
+            finalOrder[1] = 'G';
+            finalOrder[2] = 'B';
+        }
+
+        //GBC
+        potentialMoves = (moves[0][1] + moves[1][0] + moves[2][2]);
+        if(potentialMoves < finalMoves)
+        {
+            finalMoves = potentialMoves;
+            finalOrder[0] = 'G';
+            finalOrder[1] = 'B';
+            finalOrder[2] = 'C';
+        }
+
+        //GCB
+        potentialMoves = (moves[0][1] + moves[1][2] + moves[2][0]);
+        if(potentialMoves < finalMoves)
+        {
+            finalMoves = potentialMoves;
+            finalOrder[0] = 'G';
+            finalOrder[1] = 'C';
+            finalOrder[2] = 'B';
+
+        }
         printf("%s %d\n", finalOrder, finalMoves);
 
     }
