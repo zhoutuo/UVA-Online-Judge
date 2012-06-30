9a86abdc111ab40d52161eff3642a3db47f0bb72
eclipse9614 <yzt25864126@gmail.com>
2012-06-30 22:46:24 +0800

    3888	10274851	0.008	C++	2012-06-30 14:22:26

---
+++ 10189 - Minesweeper.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+char matrix[102][102];
+int dx[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+int dy[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+
+
+int count(int i, int j)
+{
+    int sum = 0;
+    for(int x = 0; x < 8; ++x)
+    {
+        if(matrix[i + dx[x]][j + dy[x]] == '*')
+        {
+            ++sum;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    int m, n;
+    int index = 1;
+
+    while(scanf("%d %d", &m, &n))
+    {
+        if(m == 0 && n == 0)
+        {
+            break;
+        }
+        else
+        {
+            if(index != 1)
+            {
+                printf("\n");
+            }
+
+        }
+
+        memset(matrix, '.', sizeof(char) * 102 * (m + 2));
+        for(int i = 1; i <= m; ++i)
+        {
+            getchar();
+            for(int j = 1; j <= n; ++j)
+            {
+                scanf("%c", &matrix[i][j]);
+            }
+        }
+
+        for(int i = 1; i <= m; ++i)
+        {
+            for(int j = 1; j <= n; ++j)
+            {
+                if(matrix[i][j] == '.')
+                {
+                    matrix[i][j] = count(i, j) + '0';
+                }
+            }
+        }
+
+        printf("Field #%d:\n", index++);
+        for(int i = 1; i <= m; ++i)
+        {
+            for(int j = 1; j <= n; ++j)
+            {
+                printf("%c", matrix[i][j]);
+            }
+            printf("\n");
+        }
+    }
+}
