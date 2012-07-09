aa2167878154368ad7750bf6ffb9fa03754ba794
eclipse9614 <yzt25864126@gmail.com>
2012-07-09 23:20:51 +0800

    Problem	  Verdict	Lang	Time	Best	Rank 10107 - What is the Median?	 Accepted	C++	0.016	0.000	75

---
+++ 10107 - What is the Median.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int nums[10000];
+
+
+int bi_search(int low, int high, int insert)
+{
+	int start = low;
+	int end = high;
+
+	while(start <  end)
+	{
+	    int mid = (start + end) >> 1;
+		int value = nums[mid];
+		if(value >= insert)
+		{
+			end = mid;
+		}
+		else
+		{
+			start = mid + 1;
+		}
+	}
+
+    return end;
+}
+
+int main()
+{
+	int index = 0;
+	int tmp;
+	while(scanf("%d", &tmp) != EOF)
+	{
+		if(index == 0)
+		{
+			nums[index++] = tmp;
+			printf("%d\n", tmp);
+		}
+		else
+		{
+
+			int pos = bi_search(0, index, tmp);
+			for(int i = index - 1; i >= pos; --i)
+			{
+				nums[i + 1] = nums[i];
+			}
+
+			nums[pos] = tmp;
+			++index;
+
+			if(index >> 1 << 1 == index)
+			{
+				int median = (nums[index >> 1] + nums[(index >> 1) - 1]) >> 1;
+				printf("%d\n", median);
+			}
+			else
+			{
+				printf("%d\n", nums[index >> 1]);
+			}
+		}
+	}
+}
