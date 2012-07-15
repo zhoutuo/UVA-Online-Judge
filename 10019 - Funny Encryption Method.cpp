7d6e20c223b501ccad4a6bef6cdb8a18e47c741e
eclipse9614 <yzt25864126@gmail.com>
2012-07-15 18:18:47 +0800

    popcount and sparse_Count

--- 10019 - Funny Encryption Method.cpp
+++ 10019 - Funny Encryption Method.cpp
@@ -1,6 +1,46 @@
 #include <cstdio>
 using namespace std;
 
+const int m1 = 0x00005555;
+const int m2 = 0x00003333;
+const int m3 = 0x00000f0f;
+const int m4 = 0x000000ff;
+
+int pop_count(int num)
+{
+    int a = num & m1;
+    int b = (num >> 1) & m1;
+    int c = a + b;
+
+    a = c & m2;
+    b = (c >> 2) & m2;
+    c = a + b;
+
+    a = c & m3;
+    b = (c >> 4) & m3;
+    c = a + b;
+
+    a = c & m4;
+    b = (c >> 8) & m4;
+    c = a + b;
+
+    return c;
+
+}
+
+int sparse_count(int num)
+{
+    int sum = 0;
+    for(int i = 1; i <= num; i <<= 1)
+    {
+        if(num & i)
+        {
+            ++sum;
+        }
+    }
+    return sum;
+}
+
 int main()
 {
 	int n;
@@ -12,14 +52,7 @@ int main()
 		int m;
 		scanf("%d", &m);
 
-		int b1 = 0;
-		for(int i = 1; i <= m; i <<= 1)
-		{
-			if(m & i)
-			{
-				++b1;
-			}
-		}
+		int b1 = pop_count(m);
 
 		int b2 = 0;
 		for(int i = 0; i < 4; ++i)
@@ -27,13 +60,7 @@ int main()
 			int last = m % 10;
 			m /= 10;
 
-			for(int j = 1; j <= last; j <<= 1)
-			{
-				if(last & j)
-				{
-					++b2;
-				}
-			}
+            b2 += sparse_count(last);
 		}
 
 		printf("%d %d\n", b1, b2);
