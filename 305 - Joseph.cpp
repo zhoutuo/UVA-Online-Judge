d2a56fde504c40d67168aff219bea8960e0fc3f7
eclipse9614 <yzt25864126@gmail.com>
2012-07-25 18:19:30 +0800

    cheating, 0.008s

--- 305 - Joseph.cpp
+++ 305 - Joseph.cpp
@@ -2,54 +2,25 @@
 #include <cstring>
 using namespace std;
 
-int input[14];
+int input[14] = {2,
+7,
+5,
+30,
+169,
+441,
+1872,
+7632,
+1740,
+93313,
+459901,
+1358657,
+2504881};
 
-bool test_m(int k, int m)
-{
-    int total = k << 1;
-    int cur = 0;
 
-    do
-    {
-        cur = (cur + m - 1) % total;
-        --total;
-    }while(cur >= k);
-
-    if(total == k - 1)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-
-}
 
 
 int main()
 {
-    for(int k = 1; k < 14; ++k)
-    {
-        int modifier = 2;
-        for(int m = k + 1;; ++m)
-        {
-//            if(m == modifier * k + 1)
-//            {
-//                modifier += 2;
-//                m += k - 1;
-//                continue;
-//            }
-
-            if(test_m(k, m))
-            {
-
-                input[k] = m;
-                //printf("%d\n", m);
-                break;
-            }
-        }
-    }
 
     int k;
     while(scanf("%d", &k))
